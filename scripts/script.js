20b153d112f204764fc8a7490dff3b402829de3a
Gangavane412 <gangawanepriyanka412@gmail.com>
2025-05-31 23:47:06 +0530

    adding loader

---
+++ scripts/script.js
@@ -0,0 +1,74 @@
+let inputImage = document.getElementById("main-img");
+let imgContainer = document.getElementById("img-container");
+let mainEditableImg = document.getElementById("img-main");
+let imgUploader = document.getElementById("img-uploader");
+let pannerSettingHandeler = document.getElementById("panner-setting-handeler");
+let settingMainPannel = document.getElementById("settings");
+let settingPannel = document.getElementById("setting-pannel");
+let loaderBody = document.getElementById("loader")
+
+// all image control settings
+let brightness = document.getElementById("Brightness");
+let contrast = document.getElementById("Contrast");
+let saturation = document.getElementById("Saturation");
+let grayscale = document.getElementById("Grayscale");
+let invert = document.getElementById("Invert");
+let sepia = document.getElementById("Sepia");
+let blurr = document.getElementById("Blur");
+
+// Function to update image filters
+function updateImageFilter() {
+    mainEditableImg.style.filter = `
+        brightness(${brightness.value || 100}%)
+        contrast(${contrast.value || 100}%)
+        saturate(${saturation.value || 100}%)
+        grayscale(${grayscale.value || 0}%)
+        invert(${invert.value || 0}%)
+        sepia(${sepia.value || 0}%)
+        blur(${blurr.value || 0}px)
+    `;
+}
+
+// Image upload event
+inputImage.addEventListener("change", () => {
+    let imgObj = inputImage.files[0];
+    if (!imgObj) return;
+    let imgUrl = URL.createObjectURL(imgObj);
+    mainEditableImg.src = imgUrl;
+
+    imgUploader.style.display = "none";
+    imgContainer.style.display = "block";
+});
+
+
+pannerSettingHandeler.addEventListener("click", () => {
+    settingMainPannel.style.display = "block";
+});
+
+settingMainPannel.addEventListener("click", () => {
+    settingMainPannel.style.display = "none";
+});
+
+
+settingPannel.addEventListener("click", (e) => {
+    e.stopPropagation();
+});
+
+
+[brightness, contrast, saturation, grayscale, invert, sepia, blurr].forEach((slider) => {
+    slider.addEventListener("input", updateImageFilter);
+});
+
+// set timeput
+window.addEventListener("load", () => {
+  
+    loaderBody.style.display = "flex";
+   
+    setTimeout(() => {
+        loaderBody.style.display = "none";
+        document.querySelector(".main-container").style.display = "block";
+    }, 2000);
+});
+
+
+
